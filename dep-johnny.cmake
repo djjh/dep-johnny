171e2b899e495e5c2491388f87a93408fbd9762b
djjh <dylanholmes206@gmail.com>
2016-04-04 20:12:29 -0700

    Adding ability to grab a dep without needing an input file.

--- dep-johnny.cmake
+++ dep-johnny.cmake
@@ -2,7 +2,7 @@ include(CMakeParseArguments)
 
 set(DEP_JOHNNY_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-function(johnny_get_dep)
+function(johnny_get_deps_from_file)
 	# parse arguments
 	set(options "")
 	set(oneValueArgs DEP_DIR
@@ -33,4 +33,35 @@ function(johnny_get_dep)
 	                        -DDEP_INPUT_FILE=${ARG_GIT_DEP_FILE}
 	                        -P ${DEP_JOHNNY_DIR}/git.cmake
 	                        VERBATIM "dfds")
+endfunction()
+
+function(johnny_get_dep)
+	# parse arguments
+	set(options "")
+	set(oneValueArgs URL
+		             BRANCH
+		             COMMIT
+		             DIR)
+	set(multiValueArgs "")
+	cmake_parse_arguments(ARG
+		                  "${options}"
+	                      "${oneValueArgs}"
+	                      "${multiValueArgs}"
+	                      ${ARGN})
+
+	# enforce required arguments
+	if ("${ARG_URL}" STREQUAL "")
+		message(FATAL_ERROR "johnny_get_dep missing required argument: URL")
+	endif()
+	if ("${ARG_DIR}" STREQUAL "")
+		message(FATAL_ERROR "johnny_get_dep missing required argument: DIR")
+	endif()
+
+	message("get that dep johnny...")
+	execute_process(COMMAND ${CMAKE_COMMAND}
+	                        -DURL=${ARG_URL}
+	                        -DBRANCH=${ARG_BRANCH}
+	                        -DCOMMIT=${ARG_COMMIT}
+	                        -DDIR=${ARG_DIR}
+	                        -P ${DEP_JOHNNY_DIR}/git.cmake)
 endfunction()
--- git.cmake
+++ git.cmake
@@ -24,36 +24,58 @@
 # set(REPOS GLFW GLAD)
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# enforce required arguments
-if ("${DEP_OUTPUT_DIR}" STREQUAL "")
-	message(FATAL_ERROR "must pass OUTPUT_DIR using -D")
-endif()
-if ("${DEP_INPUT_FILE}" STREQUAL "")
-	message(FATAL_ERROR "must pass DEP_INPUT_FILE using -D")
-endif()
+# # enforce required arguments
+# if ("${DEP_OUTPUT_DIR}" STREQUAL "")
+# 	message(FATAL_ERROR "must pass OUTPUT_DIR using -D")
+# endif()
+# if ("${DEP_INPUT_FILE}" STREQUAL "")
+# 	message(FATAL_ERROR "must pass DEP_INPUT_FILE using -D")
+# endif()
+
+# # include git utilities
+# include(${CMAKE_CURRENT_LIST_DIR}/git_utils.cmake)
+
+# # include git dependency information defined in input file
+# include(${DEP_INPUT_FILE})
+
+# # get all the dependencies
+# foreach(REPO ${REPOS})
+# 	if (NOT "${${REPO}_DIRNAME}" STREQUAL "")
+# 		set(${REPO}_DIR "${DEP_OUTPUT_DIR}/${${REPO}_DIRNAME}")
+# 	endif()
+# 	ensure_git_dep(URL "${${REPO}_URL}"
+# 		           BRANCH "${${REPO}_BRANCH}"
+# 		           COMMIT "${${REPO}_COMMIT}"
+# 		           DIR "${${REPO}_DIR}"
+# 		           SUCCESS_VARIABLE "${REPO}_SUCCESS"
+# 		           OUTPUT_VARIABLE "${REPO}_OUPUT"
+# 		           ERROR_VARIABLE "${REPO}_ERROR")
+# 	if (NOT ${${REPO}_SUCCESS})
+# 		message("${REPO}_OUTPUT: ${${REPO}_OUTPUT}")
+# 		message("${REPO}_ERROR: ${${REPO}_ERROR}")
+# 	endif()
+# endforeach()
 
 # include git utilities
 include(${CMAKE_CURRENT_LIST_DIR}/git_utils.cmake)
 
-# include git dependency information defined in input file
-include(${DEP_INPUT_FILE})
+# enforce required arguments
+if ("${URL}" STREQUAL "")
+	message(FATAL_ERROR "must pass URL using -D")
+endif()
+if ("${DIR}" STREQUAL "")
+	message(FATAL_ERROR "must pass DIR using -D")
+endif()
 
 # get all the dependencies
-foreach(REPO ${REPOS})
-	if (NOT "${${REPO}_DIRNAME}" STREQUAL "")
-		set(${REPO}_DIR "${DEP_OUTPUT_DIR}/${${REPO}_DIRNAME}")
-	endif()
-	ensure_git_dep(URL "${${REPO}_URL}"
-		           BRANCH "${${REPO}_BRANCH}"
-		           COMMIT "${${REPO}_COMMIT}"
-		           DIR "${${REPO}_DIR}"
-		           SUCCESS_VARIABLE "${REPO}_SUCCESS"
-		           OUTPUT_VARIABLE "${REPO}_OUPUT"
-		           ERROR_VARIABLE "${REPO}_ERROR")
-
-	if (NOT ${${REPO}_SUCCESS})
-		message("${REPO}_OUTPUT: ${${REPO}_OUTPUT}")
-		message("${REPO}_ERROR: ${${REPO}_ERROR}")
-	endif()
-	
-endforeach()
+ensure_git_dep(URL "${URL}"
+	           BRANCH "${BRANCH}"
+	           COMMIT "${COMMIT}"
+	           DIR "${DIR}"
+	           SUCCESS_VARIABLE "SUCCESS"
+	           OUTPUT_VARIABLE "OUPUT"
+	           ERROR_VARIABLE "ERROR")
+if (NOT ${SUCCESS})
+	message("OUTPUT: ${OUTPUT}")
+	message("ERROR: ${ERROR}")
+endif()
